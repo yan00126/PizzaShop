d5ab16d9e69f0622d21a64640c4bdae0cf83f4fb
FeiYan <yan00126@algonquinlive.com>
2024-07-30 12:30:37 -0400

    customvalidation

---
+++ Models/CheckboxItem.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaShop;
+
+public class CheckboxItem
+{
+    public string Text {get; set;} = "";
+
+    public string Value {get; set;} = "";
+
+    public bool IsChecked {get; set;}
+
+}
